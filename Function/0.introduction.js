e4e1b5c1eef95ba1d433d72a9bce17d7247c7fcc
Abraham Dulla <abrish.d143t@gmail.com>
2022-12-22 10:45:29 +0300

    declaration function

---
+++ Function/0.introduction.js
@@ -0,0 +1,3 @@
+console.log("There are 6 ways to declare javascript function");
+
+for (i = 1; i <= 6; i++) {}
---
+++ Function/declaration.js
@@ -0,0 +1,13 @@
+// Syntax
+function declaration() {
+  console.log("This is declaration function");
+}
+declaration();
+
+// eg 1 sum.
+
+// function sum(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// sum(4, 9);
